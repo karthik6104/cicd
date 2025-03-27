19acd5b315c3e4389cdad10d3b356b1ee63194f3
karthik chaganti <143590945+karthik6104@users.noreply.github.com>
2025-03-27 15:39:58 +0530

    Update integration.test.js

--- test/integration.test.js
+++ test/integration.test.js
@@ -6,9 +6,10 @@ chai.use(chaiHttp);
 
 describe('Integration Test: Nginx Web Server', () => {
   it('should return status 200 and the greeting from the HTML file', (done) => {
-    chai.request('http://localhost:8080')  // Assuming Docker container is mapped to port 8080
+    chai.request('http://localhost:8080&#39;)  // Assuming Docker container is mapped to port 8080
       .get('/')
       .end((err, res) => {
+        console.log(res);  // Debug the response object
         expect(res).to.have.status(200);  // Ensure the server returns 200 status code
         expect(res.text).to.include('Hello from Docker!');  // Ensure the HTML content is correct
         done();
